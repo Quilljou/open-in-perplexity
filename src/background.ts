bb3dea7edc572ffc8da8d14168786d12e2fa10e7
Quill <quillzhou@gmail.com>
2024-05-06 11:28:13 +0800

    feat: add firefox

--- src/background.ts
+++ src/background.ts
@@ -1,30 +1,29 @@
-const PAGE_ID = "open-in-perplexity"
-const SELECTION_ID = "open-in-perplexity-selection"
-const P_URL = 'https://www.perplexity.ai?utm_source=oip'
+import browser from 'webextension-polyfill'
 
+const PAGE_ID = "open-in-perplexity";
+const SELECTION_ID = "open-in-perplexity-selection";
+const P_URL = 'https://www.perplexity.ai';
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: SELECTION_ID,
   title: 'Perplexity "%s"',
   contexts: ['selection']
 });
 
-chrome.contextMenus.create({
+browser.contextMenus.create({
   id: PAGE_ID,
   title: 'Open Perplexity',
   contexts: ['page', 'frame']
 });
 
-
-
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === SELECTION_ID) {
-    chrome.tabs.create({ url: P_URL + `&q=${info.selectionText}` });
+    browser.tabs.create({ url: P_URL + `?q=${info.selectionText}` });
   } else if (info.menuItemId === PAGE_ID) {
-    chrome.tabs.create({ url: P_URL });
+    browser.tabs.create({ url: P_URL });
   }
 });
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: P_URL });
+browser.action.onClicked.addListener((tab) => {
+  browser.tabs.create({ url: P_URL });
 });
